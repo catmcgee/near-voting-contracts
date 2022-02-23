777d1d2131fa471c66e5f3ad8415eb5d617a1e14
Cat McGee <helloworld@mcgee.cat>
2022-02-23 13:02:57 +0000

    approval voting workoing

--- src/lib.rs
+++ src/lib.rs
@@ -58,13 +58,18 @@ impl VoteContract {
     }
 
     pub fn add_vote(&mut self, name: String) -> String{
-        
+
         if self.candidates.get(&name).is_none(){
             return "Candidate not found!".to_string();
         }
 
-        if !self.voters.get(&env::signer_account_id().to_string()).is_none() {
-            return "You already voted before!".to_string();
+        let mut thing = String::new();
+        
+        thing.push_str(&env::signer_account_id().to_string());
+        thing.push_str(&name);
+
+        if !self.voters.get(&thing).is_none() {
+            return "You already voted for this candidate before!".to_string();
         }
 
         let mut val = self.candidates.get(&name).unwrap();
@@ -72,69 +77,9 @@ impl VoteContract {
 
         self.candidates.insert(&name,&val);
 
-        self.voters.insert(&env::signer_account_id().to_string(), &name);
+        self.voters.insert(&thing.to_string(), &name);
             
         return ["You voted for",&name,"successfully.","Total votes:", &val.to_string()].join(" ");
     }
 }
-
-// impl VoteContract {
-//     // ADD CONTRACT METHODS HERE
-//     pub fn add_candidate(&mut self, name: String) -> String{
-        
-//         if !self.candidates.get(&name).is_none(){
-//             return "Candidate already in the list.".to_string();
-//         }
-
-//         self.candidates.insert(&name,&0);
-        
-//         return ["Candidate",&name,"Added."].join(" ");
-//     }
-
-//     pub fn get_candidates(&self) -> Vec<String>{
-//         return self.candidates.keys_as_vector().to_vec().clone();
-//     }
-
-//     pub fn show_results(&self) -> Vec<(String,i128)>{
-
-//         if self.voters.get(&env::signer_account_id().to_string()).is_none() {
-//             return [("You must vote first to show the results.".to_string(),0)].to_vec();
-//         }
-
-//         return self.candidates.iter().map(|x|x).collect();
-//     }
-
-//     pub fn show_voters(&self) -> Vec<(String,String)>{
-
-//         if self.voters.get(&env::signer_account_id().to_string()).is_none() {
-//             return [("You must vote first to show the voters.".to_string(),"".to_string())].to_vec();
-//         }
-
-//         return self.voters.iter().map(|x|x).collect();
-//     }
-
-//     pub fn add_vote(&mut self, names: [String]) -> String{
-        
-//         for candidates in names {
-//             println(name)
-//         }
-
-//         if self.candidates.get(&names).is_none(){
-//             return "Candidate not found!".to_string();
-//         }
-
-//         if !self.voters.get(&env::signer_account_id().to_string()).is_none() {
-//             return "You already voted before!".to_string();
-//         }
-
-//         let mut val = self.candidates.get(&name).unwrap();
-//         val += 1;
-
-//         self.candidates.insert(&name,&val);
-
-//         self.voters.insert(&env::signer_account_id().to_string(), &name);
-            
-//         return ["You voted for",&name,"successfully.","Total votes:", &val.to_string()].join(" ");
-//     }
-// }
 
